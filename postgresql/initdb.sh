a9e53258f8cefe05f827d008caa740dbfacff146
jesusalbujas <jesusramirez35000@gmail.com>
2024-09-18 10:49:44 -0400

    Add Dockerfile and PostgreSQL workflow

---
+++ postgresql/initdb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$( psql -U $POSTGRES_USER -tAc "SELECT 1 FROM pg_roles WHERE rolname='adempiere'" )" != '1' ]
+then
+    createuser -U postgres adempiere -dlrs
+    psql -U postgres -tAc "alter user adempiere password 'adempiere';"
+    createdb -U adempiere adempiere
+fi
